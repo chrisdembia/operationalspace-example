e8338011fad5d835a74e3470fb6e3f64422aad12
Christopher Dembia <cld72@cornell.edu>
2013-06-26 13:40:03 -0700

    Use robotics-like equations.

--- rrr_manipulator.m
+++ rrr_manipulator.m
@@ -82,42 +82,69 @@ function xDot = equationsOfMotion(t, x, p)
     MM23 = m3 * L2 * L3 * cos(q3 - q2);
 
     % Dynamics.
+    q12 = q1 + q2;
+    q123 = q1 + q2 + q3;
+
+    J11 = -p.L1*sin(q1) - p.L2*sin(q12) - p.L3*sin(q123);
+    J12 = -p.L2*sin(q12) - p.L3*sin(q123);
+    J13 = -p.L3*sin(q123);
+    J21 = p.L1*cos(q1) + p.L2*cos(q12) + p.L3*cos(q123);
+    J22 = p.L2*cos(q12) + p.L3*cos(q123);
+    J23 = p.L3*cos(q123);
+    Jacobian = [J11 J12 J13;
+                J21 J22 J23];
+
+    MM11 = m1*L1^2 + m2*(L1^2+L2^2+2*L1*L2*cos(q2)) + ...
+        m3*(L1^2+L2^2+L3^2+2*L1*L2*cos(q2)+2*L2*L3*cos(q3)+2*L1*L3*cos(q2+q3));
+    MM12 = L2*m2*(L2+L1*cos(q2)) + ...
+        m3*(L2^2+L3^2+L1*L2*cos(q2)+2*L2*L3*cos(q3)+L1*L3*cos(q2+q3));
+    MM13 = L3*m3*(L3+L2*cos(q3)+L1*cos(q2+q3));
+    MM22 = m2*L2^2 + m3*(L2^2+L3^2+2*L2*L3*cos(q3));
+    MM23 = L3*m3*(L3+L2*cos(q3));
+    MM33 = m3*L3^2;
+
     MassMatrix = [MM11 MM12 MM13;
                   MM12 MM22 MM23;
                   MM13 MM23 MM33];
 
-    QuadraticVelocity = [...
-        -m23 * L1*L2 * sin(q2 - q1) * q2d^2 - m3 * L1*L3 * sin(q3 - q1) * q3d^2;
-         m23 * L1*L2 * sin(q2 - q1) * q1d^2 - m3 * L2*L3 * sin(q3 - q2) * q3d^2;
-         m3 * L1*L3 * sin(q3 - q1) * q1d^2 + m3 * L2*L3 * sin(q3 - q2) * q2d^2];
+    QV1 = L1*L2*m2*sin(q2)*(q1d^2-(q1d+q2d)^2) + ...
+        m3*(L1*L2*sin(q2)*q1d^2+L1*L3*sin(q2+q3)*q1d^2+...
+        L2*L3*sin(q3)*(q1d+q2d)^2-L1*L2*sin(q2)*(q1d+q2d)^2-...
+        L2*L3*sin(q3)*(q1d+q2d+q3d)^2-L1*L3*sin(q2+q3)*(q1d+q2d+q3d)^2);
 
-    Gravity = p.g * [m123 * L1 * cos(q1);
-                     m23 * L2 * cos(q2);
-                     m3 * L3 * cos(q3)];
+    QV2 = L1*L2*m2*sin(q2)*q1d^2 + ...
+        m3*(L1*L2*sin(q2)*q1d^2+L1*L3*sin(q2+q3)*q1d^2+...
+        L2*L3*sin(q3)*(q1d+q2d)^2-L2*L3*sin(q3)*(q1d+q2d+q3d)^2);
 
-    % Control.
-    Jacobian = [-p.L1 * sin(q1), -p.L2 * sin(q2), -p.L3 * sin(q3);
-                 p.L1 * cos(q1),  p.L2 * cos(q2),  p.L3 * cos(q3)];
+    QV3 = L3*m3*(L1*sin(q2+q3)*q1d^2+L2*sin(q3)*(q1d+q2d)^2);
 
-    %{
-    %q12 = q1 + q2;
-    %q123 = q1 + q2 + q3;
-    Jacobian = [-p.L1*sin(q1) - p.L2*sin(q12) - p.L3*sin(q123), - p.L2*sin(q12) - p.L3*sin(q123), -p.L3*sin(q123);
-               [p.L1*cos(q1) + p.L2*cos(q12) + p.L3*cos(q123), p.L2*cos(q12) + p.L3*cos(q123), p.L3*cos(q123)];
-    %}
+    QuadraticVelocity = [QV1;
+                         QV2;
+                         QV3];
+
+    G1 = L1*m1*cos(q1) + m2*(L1*cos(q1)+L2*cos(q1+q2)) + ...
+        m3*(L1*cos(q1)+L2*cos(q1+q2)+L3*cos(q1+q2+q3));
+    G2 = L2*m2*cos(q1+q2) + m3*(L2*cos(q1+q2)+L3*cos(q1+q2+q3));
+    G3 = L3*m3*cos(q1+q2+q3);
+    Gravity = p.g * [G1;
+                     G2;
+                     G3];
+
 
     invMassMatrix = inv(MassMatrix);
     TaskSpaceMassMatrix = inv(Jacobian * invMassMatrix * Jacobian');
 
     DynConsistentJacInverse = invMassMatrix * Jacobian' * TaskSpaceMassMatrix;
+    %JacPseudoinverse = Jacobian' * inv(Jacobian * Jacobian');
 
     TaskSpaceGravity = DynConsistentJacInverse' * Gravity;
 
     EndEffectorForce = TaskSpaceGravity;
 
     % TODO Nullspace action.
+    Nullspace = eye(3) - DynConsistentJacInverse * Jacobian;
 
-    Actuators = Jacobian' * EndEffectorForce;
+    Actuators = Jacobian' * EndEffectorForce + Nullspace' * ;
 
 
     % Solve.
